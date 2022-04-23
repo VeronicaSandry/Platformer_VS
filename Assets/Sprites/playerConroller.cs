37187f6b8f2db8e0e6cd869436e8d0ed923e3012
Victorfromcyberschool <viktormlnichenko21@gmail.com>
2022-04-23 17:55:45 +1000

    ...

--- Assets/Sprites/playerConroller.cs
+++ Assets/Sprites/playerConroller.cs
@@ -3,16 +3,24 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class playerConroller : MonoBehaviour
-{
+{ 
+    Rigidbody2D rb; 
+    public float velocity;
     // Start is called before the first frame update
     void Start()
     {
-        
+        rb = GetComponent<Rigidbody2D>();  
     }
 
     // Update is called once per frame
     void Update()
     {
         
+    } 
+
+
+    void FixedUpdate() 
+    {
+        rb.velocity = new Vector2(Input.GetAxis("Horizontal") * velocity, rb.velocity.y);
     }
 }
